natto1784/meta-arago

commit fb7c466684d715bc5b3be78e0504c26fee363d3e

packagegroup: arago-base: Include lpm-demo in the default filesystem
lpm-demo package installs a copy of the tisdk-tiny-image.cpio in
the /boot directory of default filesystem.

Include this package for am62xx-evm

meta-arago-distro/recipes-core/packagegroups/packagegroup-arago-base.bb
@@ -43,6 +43,10 @@ ARAGO_EXTRA_append_keystone = "\
     k2-fw-initrd \
 "
 
+ARAGO_EXRA_append_am62xx-evm = "\
+    lpm-demo \
+"
+
 # minimal set of packages - needed to boot
 RDEPENDS_${PN} = "\
     ${@bb.utils.contains('DISTRO_FEATURES', 'zeroconf', 'packagegroup-base-zeroconf', '',d)} \

commit dedd29db2c3153400ebd3bc1a90f62fc4fd90b1f

packagegroup: arago-base: Fix typy in ARAGO_EXTRA
Use the variable ARAGO_EXTRA for override

Fixes: fb7c46668 (packagegroup: arago-base: Include lpm-demo in the default filesystem)

Signed-off-by: Nikhil Devshatwar <nikhil.nd@ti.com>

meta-arago-distro/recipes-core/packagegroups/packagegroup-arago-base.bb
@@ -43,7 +43,7 @@ ARAGO_EXTRA_append_keystone = "\
     k2-fw-initrd \
 "
 
-ARAGO_EXRA_append_am62xx-evm = "\
+ARAGO_EXTRA_append_am62xx-evm = "\
     lpm-demo \
 "
 